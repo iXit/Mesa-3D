b9c34e568e97ae312a2bb9444e0058416b82fd1f
Axel Davy <axel.davy@ens.fr>
2015-05-01 19:08:37 +0200

    egl/swrast: enable config extension for swrast
    Enables to use dri config for swrast, like vblank_mode.
    
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- src/mesa/drivers/dri/swrast/swrast.c
+++ src/mesa/drivers/dri/swrast/swrast.c
@@ -942,6 +942,7 @@ static const __DRIextension *swrast_driver_extensions[] = {
     &driCoreExtension.base,
     &driSWRastExtension.base,
     &driCopySubBufferExtension.base,
+    &dri2ConfigQueryExtension.base,
     &swrast_vtable.base,
     NULL
 };

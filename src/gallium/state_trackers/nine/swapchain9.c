f1375539c51256da951a401a968c7a96f001a3b9
Patrick Rudolph <siro@das-labor.org>
2015-10-22 17:00:35 +0200

    st/nine: set backbuffer to NULL
    Return D3DERR_INVALIDCALL instead of E_POINTER.
    On error set ppBackBuffer to NULL.
    
    Fixes failing WINE tests device.c test_swapchain().
    
    Signed-off-by: Patrick Rudolph <siro@das-labor.org>

--- src/gallium/state_trackers/nine/swapchain9.c
+++ src/gallium/state_trackers/nine/swapchain9.c
@@ -904,8 +904,11 @@ NineSwapChain9_GetBackBuffer( struct NineSwapChain9 *This,
     DBG("GetBackBuffer: This=%p iBackBuffer=%d Type=%d ppBackBuffer=%p\n",
         This, iBackBuffer, Type, ppBackBuffer);
     (void)user_error(Type == D3DBACKBUFFER_TYPE_MONO);
+    user_assert(ppBackBuffer != NULL, D3DERR_INVALIDCALL);
+    /* in case of error return NULL */
+    *ppBackBuffer = NULL;
+
     user_assert(iBackBuffer < This->params.BackBufferCount, D3DERR_INVALIDCALL);
-    user_assert(ppBackBuffer != NULL, E_POINTER);
 
     NineUnknown_AddRef(NineUnknown(This->buffers[iBackBuffer]));
     *ppBackBuffer = (IDirect3DSurface9 *)This->buffers[iBackBuffer];
